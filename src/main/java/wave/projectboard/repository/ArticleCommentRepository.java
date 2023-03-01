3f6697ae7183182f7ad54552f16a3d59af4881ce
waveofmymind <sjun0913@gmail.com>
2023-03-01 18:52:31 +0900

    #1 - data rest 설정
    게시글 댓글의 api를 rest하도록 구현

--- src/main/java/wave/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/wave/projectboard/repository/ArticleCommentRepository.java
@@ -1,7 +1,9 @@
 package wave.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import wave.projectboard.domain.ArticleComment;
+@RepositoryRestResource
 
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
 }
--- src/main/java/wave/projectboard/repository/ArticleRepository.java
+++ src/main/java/wave/projectboard/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package wave.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import wave.projectboard.domain.Article;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
