259b6e11ebfe914ad2ac42abefb3dad82449d3d5
waveofmymind <sjun0913@gmail.com>
2023-03-01 12:22:29 +0900

    #11 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로
    엔티티 클래스에 들어가는 요소인데,
    도메인과 연관이 없어 추출이 가능할 것 같아
    @MappedSuperclass를 이용하여 추출했다.
    
    그외 @DateTimeFormat 요소를 추가했다.

--- src/main/java/wave/projectboard/domain/ArticleComment.java
+++ src/main/java/wave/projectboard/domain/ArticleComment.java
@@ -19,7 +19,7 @@ import java.util.Objects;
 })
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ArticleComment {
+public class ArticleComment extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,19 +31,6 @@ public class ArticleComment {
     @Setter
     @Column(nullable = false,length = 500)
     private String content; //본문
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt; //생성일시
-    @CreatedBy
-    @Column(nullable = false,length = 100)
-    private String createdBy; //생성자
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt; //수정일시
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy; //수정자
 
     public ArticleComment(Article article, String content) {
         this.article = article;
