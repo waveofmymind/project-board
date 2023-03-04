6d8027357651406ba91380f0e5d9916dec1ad548
waveofmymind <sjun0913@gmail.com>
2023-03-05 08:46:47 +0900

    03-06 게시판 페이지 구현

--- src/main/java/wave/projectboard/controller/ArticleController.java
+++ src/main/java/wave/projectboard/controller/ArticleController.java
@@ -1,27 +1,43 @@
 package wave.projectboard.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import wave.projectboard.domain.type.SearchType;
+import wave.projectboard.dto.response.ArticleResponse;
+import wave.projectboard.dto.response.ArticleWithCommentsResponse;
+import wave.projectboard.service.ArticleService;
 
 import java.util.List;
-
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/articles")
 public class ArticleController {
 
+    private final ArticleService articleService;
+
     @GetMapping
-    public String articles(ModelMap map) {
-        map.addAttribute("articles", List.of());
+    public String articles(ModelMap map,
+                          @PageableDefault(size=10,sort="createdAt",direction = Sort.Direction.DESC) Pageable pageable,
+                           @RequestParam(required = false)SearchType searchType,
+                           @RequestParam(required = false) String searchValue) {
+
+        map.addAttribute("articles", articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
-    public String articles(@PathVariable Long articleId, ModelMap map) {
-        map.addAttribute("article", null);
-        map.addAttribute("articleComment", List.of());
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.articleCommentsResponse());
 
         return "articles/detail";
     }
