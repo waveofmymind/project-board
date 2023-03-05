a2a2d32332c04ff022f608e270a216b8c343d666
waveofmymind <sjun0913@gmail.com>
2023-03-05 19:03:05 +0900

    03-10 게시판 페이징 구현 2

--- src/main/java/wave/projectboard/controller/ArticleController.java
+++ src/main/java/wave/projectboard/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package wave.projectboard.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +15,7 @@ import wave.projectboard.domain.type.SearchType;
 import wave.projectboard.dto.response.ArticleResponse;
 import wave.projectboard.dto.response.ArticleWithCommentsResponse;
 import wave.projectboard.service.ArticleService;
+import wave.projectboard.service.PaginationService;
 
 import java.util.List;
 @RequiredArgsConstructor
@@ -22,14 +24,19 @@ import java.util.List;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(ModelMap map,
                           @PageableDefault(size=10,sort="createdAt",direction = Sort.Direction.DESC) Pageable pageable,
                            @RequestParam(required = false)SearchType searchType,
                            @RequestParam(required = false) String searchValue) {
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers= paginationService.getPaginationBarNumbers(pageable.getPageNumber(),articles.getTotalPages());
 
-        map.addAttribute("articles", articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
+        map.addAttribute("articles",articles );
+        map.addAttribute("paginationBarNumbers",barNumbers);
+
         return "articles/index";
     }
 
